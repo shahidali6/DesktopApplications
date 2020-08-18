97a08ee64a6d2ffc600c423fc4672e37aadd69ef
Shahid Saddique <shahidali6@gmail.com>
2020-08-18 11:52:14 +0500

    YouTube URL extract functions added.

--- CodeSnippetCSharp/Form1.Designer.cs
+++ CodeSnippetCSharp/Form1.Designer.cs
@@ -28,12 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.tbRawData = new System.Windows.Forms.TextBox();
             this.tbResultData = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -61,6 +64,7 @@
             this.tbRawData.Name = "tbRawData";
             this.tbRawData.Size = new System.Drawing.Size(350, 347);
             this.tbRawData.TabIndex = 2;
+            this.tbRawData.Text = resources.GetString("tbRawData.Text");
             // 
             // tbResultData
             // 
@@ -90,11 +94,33 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Extract YouTube URLs from Text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(461, 388);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(162, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Extract YouTube URLs from File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(750, 428);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.tbResultData);
@@ -116,6 +142,8 @@
         private System.Windows.Forms.TextBox tbResultData;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- CodeSnippetCSharp/Form1.cs
+++ CodeSnippetCSharp/Form1.cs
@@ -31,5 +31,23 @@ namespace CodeSnippetCSharp
         {
             tbResultData.Text = HTMLCodeSnippet.RemoveHTMLTagsFromStringNewLine(tbRawData.Text);
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            tbResultData.Text = string.Join(Environment.NewLine, HTMLCodeSnippet.ExtractYouTubeVideoURLs(tbRawData.Text));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string fileContent = string.Empty;
+            fileContent = HTMLCodeSnippet.ReadTextFileUsingDialougeBox();
+            tbResultData.Text = string.Join(Environment.NewLine, HTMLCodeSnippet.ExtractYouTubeVideoURLs(fileContent));
+            tbRawData.Text = "Extract URL using file";
+        }
     }
 }
