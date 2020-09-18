dd05c79b624e230d1c1b7750a45b1e963690c69f
Shahid Saddique <shahidali6@gmail.com>
2020-09-18 10:50:21 +0500

    Refactor Code.

---
+++ CodeSnippetCSharp/Projects/HTML/ScrapeWebSites/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapWebSites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlStringAsync();
+
+            Console.ReadLine();
+        }
+
+        private static void HtmlStringAsync()
+        {
+            var url = "http://unity3d-assetstore.blogspot.com/2018/05/unity3d-assets-collection.html";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url);
+
+            Console.WriteLine(html.Result);
+        }
+    }
+}
---
+++ CodeSnippetCSharp/Projects/HTML/Selenium1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Selenium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(12, 428);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(83, 13);
+            this.lbStatus.TabIndex = 0;
+            this.lbStatus.Text = "Status Message";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Initilize Web Driver";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbStatus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
